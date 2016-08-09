cf1b6b2c9d3941ab1508d1a5ca13e4e202788087
Matheus DallRosa <matheusdallrosa94@gmail.com>
2016-08-08 22:13:41 -0300

    Pra não esquecer

--- AES.py
+++ AES.py
@@ -1,10 +1,31 @@
 from Crypto.Cipher import AES
+from sys import argv
 
-aes = AES.new('chave de 16 byte',AES.MODE_CBC,'1111111111111111')
-mensagem = "mensagem de 16 b"
-encript = aes.encrypt(mensagem)
+def fill16Bytes(text):
+	textLen = len(text)
+	while textLen % 16 != 0:
+		text += 'P'
+		textLen += 1
+	return text
 
-aes2 = AES.new('chave de 16 byte',AES.MODE_CBC,'1111111111111111')
-decript = aes2.decrypt(encript)
-print(mensagem == decript)
-print(decript)
+def myEncrypt(text,key):
+	aes = AES.new(key,AES.MODE_CBC,'1111111111111111')
+	return aes.encrypt(text)
+
+def myDecrypt(text,key):
+	aes2 = AES.new(key,AES.MODE_CBC,'1111111111111111')
+	return aes2.decrypt(encript)
+
+def main():
+	argc = len(argv);
+	if argc != 4:
+		print("Entrada incorreta leia o readme.")
+		return
+
+	if(argv[1] == "encrypt"):
+		print("encriptar")
+	elif(argv[1] == "decrypt"):
+		print("decriptar")
+	else:
+		print("Opcao invalida leia o readme.");
+main()
