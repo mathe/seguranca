527f4e78da0c77e62d8307aeb2e744098970b29e
Matheus DallRosa <matheusdallrosa94@gmail.com>
2016-08-08 22:50:57 -0300

    Utilizando AES.

--- AES.py
+++ AES.py
@@ -8,24 +8,26 @@ def fill16Bytes(text):
 		textLen += 1
 	return text
 
-def myEncrypt(text,key):
+def myEncrypt(key,text):
 	aes = AES.new(key,AES.MODE_CBC,'1111111111111111')
 	return aes.encrypt(text)
 
-def myDecrypt(text,key):
+def myDecrypt(key,text):
 	aes2 = AES.new(key,AES.MODE_CBC,'1111111111111111')
-	return aes2.decrypt(encript)
+	return aes2.decrypt(text)
 
 def main():
-	argc = len(argv);
-	if argc != 4:
-		print("Entrada incorreta leia o readme.")
-		return
-
+	srcKey = open(argv[2]);
+	srcText = open(argv[3]);
+	destText = open(argv[4],'w');
+	
+	key = fill16Bytes(srcKey.read())
+	text = fill16Bytes(srcText.read())	
 	if(argv[1] == "encrypt"):
-		print("encriptar")
+		destText.write(myEncrypt(key,text))
 	elif(argv[1] == "decrypt"):
-		print("decriptar")
-	else:
-		print("Opcao invalida leia o readme.");
+		destText.write(myDecrypt(key,text))
+	srcKey.close()
+	srcText.close()
+	destText.close()
 main()
