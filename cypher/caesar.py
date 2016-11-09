062fe234f6ff6e646d01d9b3bf6ead5294829a23
Matheus DallRosa <matheusdallrosa94@gmail.com>
2016-11-09 11:00:08 -0200

    Tamanho do Alfabeto

--- cypher/caesar.py
+++ cypher/caesar.py
@@ -11,16 +11,21 @@ def caesar(src,dest,shift):
     text_src = src.read()
     text_dest = ""
     for t in text_src:
-        rt = (ord(t)-ord('a')+shift+256) % 256
+        rt = (ord(t)-ord('a')+shift+26) % 26
         text_dest += chr(ord('a')+rt)
     dest.write(text_dest)
 
 def main():
-    if len(argv) != 4:
+    if len(argv) != 5:
         print("Argumentos incorretos, leia o README.md")
         return 0;
-    (src,dest,shift) = open(argv[2],"r"),open(argv[3],"w"),int(argv[1])
-    caesar(src,dest,shift)
+    (src,dest,shift) = open(argv[3],"r"),open(argv[4],"w"),int(argv[2])
+    if argv[1] == "-e":
+        caesar(src,dest,shift)
+    elif argv[1] == "-d":
+        caesar(src,dest,(-1)*shift)
+    else:
+        print("Opcao Invalida")
     src.close()
     dest.close()
 
